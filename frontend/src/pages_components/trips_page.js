ff29da7b575608b17ea885f3ee2b17859871a000
TRISCOS Simon <Simon.Triscos@bordeaux-inp.fr>
2021-10-11 18:45:33 +0200

    readded trip creation button

--- frontend/src/pages_components/trips_page.js
+++ frontend/src/pages_components/trips_page.js
@@ -1,15 +1,76 @@
-import React, { Component } from "react";
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Typography from '@mui/material/Typography';
+import Button from '@material-ui/core/Button';
+import NewTripDialog from "./new_trip_dialog"
 
-class Trips extends Component {
 
-  render() {
+function Main(props) {
+
+    const [newTripDialogOpen, setNewTripialogOpen] = useState(false);
+
+    const handleOpen = () => {
+        setNewTripialogOpen(true)
+    }
+
+    const handleClose = () => {
+        setNewTripialogOpen(false)
+    }
+
+    const rows_tmp = [
+        {id: 0, name: "Nydala", description: "Trip to the lake", date: "01/01/2022", duration: "2 days"}, 
+        {id: 1, name: "Climbing Mont Fuji", description: "Travel to Japan", date: "30/10/2021", duration: "2 weeks"},
+        {id: 2, name: "Hiking", description: "Hiking in sweden", date: "17/03/2022", duration: "2 days"}
+    ]
+
 
     return (
-      <div>
-        <p> MY TRIPS PAGE </p>
-      </div>
+
+            <main>
+
+                <div className="newTripButton">
+                    <Button onClick={handleOpen} variant="contained" color="inherit" >Create Trip</Button>
+
+                    {newTripDialogOpen && 
+                        <NewTripDialog handleClose={handleClose} />}
+                </div>
+
+                <div className="tripList">
+                    {rows_tmp.map((row) => {
+                        return (
+                            <div className="tripCard">
+                                <Card sx={{ display: 'flex', backgroundColor: 'rgb(156, 156, 247)' }}>
+                                    <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+                                        <CardContent sx={{ flex: '1 0 auto' }}>
+                                            <Typography component="div" variant="h5">
+                                                {row.name}
+                                            </Typography>
+                                            <Typography variant="subtitle1" color="text.secondary" >
+                                                {row.description}
+                                            </Typography>
+                                            <Typography variant="subtitle1" color="text.secondary" component="div">
+                                                {row.date}
+                                            </Typography>
+                                            <Typography variant="subtitle1" color="text.secondary" component="div">
+                                                {row.duration}
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button size="small">Share</Button>
+                                        </CardActions>
+                                        
+                                    </Box>
+                                </Card>
+                            </div>
+                        );
+                    })}
+                </div> 
+            </main> 
     );
-  }
+  
 }
 
-export default Trips
+export default Main
