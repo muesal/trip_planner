aee11c1a6e6504dbb34562684687b3cb67697367
jmrm0038 <jmrm0038@red.ujaen.es>
2021-10-19 09:26:18 +0200

    Fixed checklist style and css

--- frontend/src/pages_components/checklist_page.js
+++ frontend/src/pages_components/checklist_page.js
@@ -3,8 +3,11 @@
 import React, { useState, useEffect } from 'react';
 import { JsonForms } from '@jsonforms/react';
 import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 import { DataGrid } from "@material-ui/data-grid";
+import { Box } from '@mui/system';
 
 import { materialRenderers, materialCells } from '@jsonforms/material-renderers';
 import axios from 'axios';
@@ -13,141 +16,190 @@ import { schema, uiSchema } from './create_schema'
 
 function Checklists(props) {
 
-    const [data, setData] = useState();
-    const [trips, setTrips] = useState(null);
-    const [rows, setRows] = useState(null)
-    const apiData = props.apiData;
-    const loading = props.loading;  
+  const [data, setData] = useState();
+  const [trips, setTrips] = useState(null);
+  const [rows, setRows] = useState(null)
+  const apiData = props.apiData;
+  const loading = props.loading;
+  const [value, setValue] = useState(0);
 
-    useEffect(() => {
-        getTrips();
-        setRows(hardcoded_rows)
-    }, [])
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-    const hardcoded_rows = [{id: 0, name: "mayonnaise", quantity: 2}, {id: 1, name: "chicken", quantity: 1}]
+  useEffect(() => {
+    getTrips();
+    setRows(hardcoded_rows)
+  }, [])
 
-    const columns = [
-        { field: "name", headerName: "Item", flex: 1 },
-        { field: "quantity", headerName: "Quantity", flex: 0.5 },
-    ];
+  const hardcoded_rows = [{ id: 0, name: "mayonnaise", quantity: 2 }, { id: 1, name: "chicken", quantity: 1 }, { id: 2, name: "chicken", quantity: 1 }, { id: 3, name: "chicken", quantity: 1 }, { id: 4, name: "chicken", quantity: 1 }, { id: 5, name: "chicken", quantity: 1 }]
 
-    const handleSubmit = async () => {
-       
-        if(data && data.rsName && data.rsCat) {
 
-            let bodyFormData = new FormData();
-            bodyFormData.append("rsName", data.rsName);
-            bodyFormData.append("rsCat", data.rsCat);
+  const columns = [
+    { field: "name", headerName: "Item", flex: 1 },
+    { field: "quantity", headerName: "Quantity", flex: 0.5 },
+  ];
 
-            axios({
-                method: "post",
-                url: "http://127.0.0.1:5000/add-resource",
-                data: bodyFormData,
-                headers: { "Content-Type": "multipart/form-data" },
-            })
-                .then((res) => {
-                    console.log(res);
-                    props.getData()
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
+  const handleSubmit = async () => {
 
-        }
-        
+    if (data && data.rsName && data.rsCat) {
+
+      let bodyFormData = new FormData();
+      bodyFormData.append("rsName", data.rsName);
+      bodyFormData.append("rsCat", data.rsCat);
+
+      axios({
+        method: "post",
+        url: "http://127.0.0.1:5000/add-resource",
+        data: bodyFormData,
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+        .then((res) => {
+          console.log(res);
+          props.getData()
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
     }
 
-    const getTrips = () => {
-        axios({
-                method: "get",
-                url: "http://127.0.0.1:5000/trips",
-                data: {},  // TODO: add userID
-                headers: { "Content-Type": "application/json" },
-            })
-                .then((res) => {
-                    setTrips(res.data)
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
- 
-    }
+  }
+
+  const getTrips = () => {
+    axios({
+      method: "get",
+      url: "http://127.0.0.1:5000/trips",
+      data: {},  // TODO: add userID
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => {
+        setTrips(res.data)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+  }
+  function TabPanel(props) {
+    const { children, value, index, ...other } = props;
 
     return (
-
-        <div className="checklistPage">
-            {<div className="checklistMenu">
-                <Grid item  container direction="column" justifyContent="flex-end" style={{paddingTop:'2%'}}>
-
-                    {trips && 
-                        trips.map((row) => {
-                            return (
-                                <div className="tripButton" key={row.id}>
-                                    <Button  style={{width:200}} color="primary" variant="outlined"> {row.name} </Button>
-                                </div>
-                            );
-                    })}
-                </Grid>
-            </div>}
-
-
-          
-                <div className="Checklist"> 
-
-                    <DataGrid
-                        rows={rows || []}
-                        columns={columns}
-                        disableColumnMenu={true}
-                        checkboxSelection={true}
-                        autoHeight
-                        
-                    />
-
-                </div> 
-
-                <div className="addResources">
-
-                    <h2> Add Resource </h2>
-
-                    <JsonForms
-                        data={data}
-                        schema={schema}
-                        uischema={uiSchema}
-                        renderers={materialRenderers}
-                        onChange={({ data, errors }) => {setData(data);}}
-                        cells={materialCells}
-                    />
-
-                    <Button onClick={handleSubmit} style={{margin:'1%'}} variant="outlined" color="default" >SUBMIT</Button>
-                    
-
-                    {loading === true ? (
-                        <div>
-                            <h2>Loading...</h2>
-                        </div>
-                    ) : (
-                        <section>
-                            {apiData.map((rs) => {
-                                const rsId = rs[0];
-                                const rsName = rs[1];
-                                const rsCat = rs[2];
-
-                                return (
-                                    <div className="rs-container" key={String(rsId)}>
-                                        <h2>{rsName}</h2>
-                                        <p>
-                                            <strong>Category:</strong> {rsCat}
-                                        </p>
-                                    </div>
-                                );
-                            })}
-                        </section>
-                    )}
-                </div>
-            
-        </div>
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`vertical-tabpanel-${index}`}
+        aria-labelledby={`vertical-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ paddingLeft: "22vw", mt: 5 }}>
+            {children}
+          </Box>
+        )}
+      </div>
     );
-  
+  }
+
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+
+  function a11yProps(index) {
+    return {
+      id: `vertical-tab-${index}`,
+      'aria-controls': `vertical-tabpanel-${index}`,
+    };
+  }
+
+
+  return (
+
+    <Box alignSelf='flex-start' alignContent='center'
+      sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', maxHeight: "80vh", width: 1 }}
+    >
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        sx={{ borderRight: 1, borderColor: 'divider' }}
+      >
+        <Tab label="My list" {...a11yProps(0)} />
+        {trips &&
+          trips.map((row) => {
+            return (
+              <Tab label={row.name} {...a11yProps(row.id)} />
+            );
+          })}
+      </Tabs>
+
+      <TabPanel value={value} index={0} sx={{ paddingLeft: 240 }} >
+        <DataGrid
+          rows={rows || []}
+          columns={columns}
+          disableColumnMenu={true}
+          checkboxSelection={true}
+          autoHeight
+        />
+      </TabPanel>
+      {trips &&
+        trips.map((row) => {
+          return (
+            <TabPanel value={value} index={row.id}>
+              <DataGrid
+                rows={rows || []}
+                columns={columns}
+                disableColumnMenu={true}
+                checkboxSelection={true}
+                autoHeight
+              />
+            </TabPanel>
+          );
+        })}
+      <Box sx={{paddingLeft: "10vw", mt: 5}}>
+
+        <h2> Add Item </h2>
+
+        <JsonForms
+          data={data}
+          schema={schema}
+          uischema={uiSchema}
+          renderers={materialRenderers}
+          onChange={({ data, errors }) => { setData(data); }}
+          cells={materialCells}
+        />
+
+        <Button onClick={handleSubmit} style={{ margin: '1%' }} variant="outlined" color="default" >SUBMIT</Button>
+
+        {loading === true ? (
+          <div>
+            <h2>Loading...</h2>
+          </div>
+        ) : (
+          <section >
+            {apiData.map((rs) => {
+              const rsId = rs[0];
+              const rsName = rs[1];
+              const rsCat = rs[2];
+
+              return (
+                <div className="rs-container" key={String(rsId)}>
+                  <h2>{rsName}</h2>
+                  <p>
+                    <strong>Category:</strong> {rsCat}
+                  </p>
+                </div>
+              );
+            })}
+          </section>
+        )}
+      </Box>
+    </Box>
+  );
+
 }
 
 export default Checklists
